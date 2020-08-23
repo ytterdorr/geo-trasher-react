278943cf437cf25be4e1f49122011d628384f618
Fabian Isaksson <fabi.isak@gmail.com>
2020-08-23 22:03:06 +0200

    fix s3 adress

--- src/views/FirstView.js
+++ src/views/FirstView.js
@@ -1,32 +1,32 @@
-import React from "react";
-import DataDisplay from "../components/DataDisplay/DataDisplay";
+import React from 'react';
+import DataDisplay from '../components/DataDisplay/DataDisplay';
 // import Card from "../components/Card/Card";
-import SigninSignup from "../components/SigninSignup/SigninSignup";
-import Colors from "../styles/Colors";
-import logoImg from "../images/geoTrasherLogoTransBackground.png";
+import SigninSignup from '../components/SigninSignup/SigninSignup';
+import Colors from '../styles/Colors';
+import logoImg from '../images/geoTrasherLogoTransBackground.png';
 // What do I Want to present in this first view?
 // Data-display
 //
 
 const FirstView = (props) => {
   const handleSessionButton = () => {
-    props.updateView("SessionView");
+    props.updateView('SessionView');
   };
 
   const handleShowInstructions = () => {
-    props.updateView("InstructionsView");
+    props.updateView('InstructionsView');
   };
 
   const handleMyPageButton = () => {
-    props.updateView("UserView");
+    props.updateView('UserView');
   };
   return (
-    <div className="view-container">
+    <div className='view-container'>
       {/* <h1>GeoTrasher!</h1> */}
-      <img src={logoImg} style={{ maxWidth: "80vw", marginBottom: "2vh" }} />
+      <img src={logoImg} style={{ maxWidth: '80vw', marginBottom: '2vh' }} />
       <DataDisplay
         serverHost={props.serverHost}
-        sessionID="0"
+        sessionID='0'
         style={myStyles.CardStyle}
       />
       {localStorage.token ? (
@@ -40,11 +40,12 @@ const FirstView = (props) => {
       <button style={myStyles.Button} onClick={handleSessionButton}>
         Anonymous session
       </button>
-      <a href="#" onClick={handleShowInstructions}>
+      <a href='#' onClick={handleShowInstructions}>
         How to use this page
       </a>
       <p>This is a work in progress, made by Tore Haglund. </p>
-      <a href="mailto: tore.haglund@gmail.com">Send Feedback</a>
+      <a href='mailto: tore.haglund@gmail.com'>Send Feedback</a>
+      <p>( Now with continuous integration :D )</p>
     </div>
   );
 };
@@ -52,15 +53,15 @@ const FirstView = (props) => {
 const myStyles = {
   CardStyle: {
     backgroundColor: Colors.defaultBackground,
-    width: "150px",
-    maxwidth: "80%",
-    marginBottom: "10px",
-    padding: "10px",
+    width: '150px',
+    maxwidth: '80%',
+    marginBottom: '10px',
+    padding: '10px',
   },
   Button: {
     backgroundColor: Colors.blue,
-    padding: "5px",
-    marginBottom: "10px",
+    padding: '5px',
+    marginBottom: '10px',
   },
 };
 
